304b05b3036fd60eb2f3f68d8592b8af6c31da08
Phil <philip.schaefer0@gmail.com>
2019-08-21 12:21:16 +0200

    removed bot-name prefix

--- kiri-chat-bot/index.js
+++ kiri-chat-bot/index.js
@@ -38,14 +38,14 @@ client.commands = new Discord.Collection();
 fs.readdir('./kiri-chat-bot/commands/', (err, files) => {
   if (err) console.error(err);
   let jsfiles = files.filter(f => f.split('.').pop() === 'js');
-  if (jsfiles.length <= 0) return console.log(`[${config.name}] No CMD(s) to load!`);
-  console.log(`[${config.name}] Loading ${jsfiles.length} command(s)...`);
+  if (jsfiles.length <= 0) return console.log('No CMD(s) to load!');
+  console.log(`Loading ${jsfiles.length} command(s)...`);
   jsfiles.forEach((f, i) => {
     let probs = require(`../kiri-chat-bot/commands/${f}`);
-    console.log(`[${config.name}]     ${i + 1}) Loaded: ${f}!`);
+    console.log(`    ${i + 1}) Loaded: ${f}!`);
     client.commands.set(probs.help.name, probs);
   });
-  console.log(`[${config.name}] Loaded ${jsfiles.length} command(s)!`);
+  console.log(`Loaded ${jsfiles.length} command(s)!`);
 });
 
 // function lister
@@ -53,23 +53,23 @@ client.functions = new Discord.Collection();
 fs.readdir('./kiri-chat-bot/functions/', (err, files) => {
   if (err) console.error(err);
   let jsfiles = files.filter(f => f.split('.').pop() === 'js');
-  if (jsfiles.length <= 0) return console.log(`[${config.name}] No function(s) to load!`);
-  console.log(`[${config.name}] Loading ${jsfiles.length} function(s)...`);
+  if (jsfiles.length <= 0) return console.log('No function(s) to load!');
+  console.log(`Loading ${jsfiles.length} function(s)...`);
   jsfiles.forEach((f, i) => {
     let probs = require(`../kiri-chat-bot/functions/${f}`);
-    console.log(`[${config.name}]     ${i + 1}) Loaded: ${f}!`);
+    console.log(`    ${i + 1}) Loaded: ${f}!`);
     client.functions.set(probs.help.name, probs);
   });
-  console.log(`[${config.name}] Loaded ${jsfiles.length} function(s)!`);
+  console.log(`Loaded ${jsfiles.length} function(s)!`);
 });
 
 client.on('ready', async () => {
   // confirm user logged in
-  console.log(`[${config.name}] Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as ${client.user.tag}!`);
 
   // set bot player status
   client.functions.get('setup_status').run(client, fs, config)
-    .then(() => console.log(`[${config.name}] Set status!`));
+    .then(() => console.log('Set status!'));
 });
 
 client.on('message', async (message) => {
