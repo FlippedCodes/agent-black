6359bcdba1f52a17df6aae9f2a73fd7ce869a66a
Phil <philip.schaefer0@gmail.com>
2019-10-30 22:56:46 +0100

    cleanup and DB entry creation

--- functions/EVENT_guildBanAdd.js
+++ functions/EVENT_guildBanAdd.js
@@ -1,17 +1,17 @@
-// module.exports.run = async (client, guild, user, DB, config) => {
+const Ban = require('../database/models/Ban');
+
 module.exports.run = async (client, guild, user, config) => {
   // getting infomration needed
-  const userID = user.id;
-  const userTag = user.tag;
-  const serverID = guild.id;
   // getting newly added ban
   guild.fetchBan(user)
     .then(async (ban) => {
-      console.log(userID);
-      console.log(userTag);
-      console.log(serverID);
-      console.log(ban.reason);
-      // TODO: create DB entry
+      const userID = user.id;
+      const userTag = user.tag;
+      const serverID = guild.id;
+      const reason = ban.reason;
+      await Ban.create({
+        userID, serverID, userTag, reason,
+      }).catch((err) => console.error(err));
     });
 };
 
