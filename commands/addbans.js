63865601f76cd18f7e70c7597cfe7612708ac1a5
Phil <philip.schaefer0@gmail.com>
2019-10-30 23:42:02 +0100

    remake with sequelize

--- commands/addbans.js
+++ commands/addbans.js
@@ -1,22 +1,31 @@
 const { RichEmbed } = require('discord.js');
 
-module.exports.run = async (client, message, args, DB, config) => {
+const Ban = require('../database/models/Ban');
+
+module.exports.run = async (client, message, args, config) => {
   if (message.author.id !== '172031697355800577') return message.react('❌');
-  message.channel.send({ embed: new RichEmbed().setAuthor('Processing banns') })
+  message.channel.send({ embed: new RichEmbed().setAuthor('Processing banns...') })
     .then((msg) => {
       message.guild.fetchBans(true)
         .then(async (bans) => {
-          bans.forEach(({ user, reason }) => {
-            DB.query(`SELECT * FROM bannedUsers WHERE userID = '${user.id}' AND serverID = '${message.guild.id}'`, (err, rows) => {
-              if (err) throw err;
-              let fixedReason = reason;
-              if (reason !== null) fixedReason = reason.replace(new RegExp('\'', 'g'), '`');
-              if (rows[0]) {
-                DB.query(`UPDATE bannedUsers SET reason = '${fixedReason}' WHERE userID = '${user.id}' AND serverID = '${message.guild.id}'`);
-              } else {
-                DB.query(`INSERT INTO bannedUsers (userID, serverID, reason, time) VALUES ('${user.id}', '${message.guild.id}', '${fixedReason}', '')`);
-              }
-            });
+          bans.forEach(async ({ user, reason }) => {
+            let fixedReason = reason;
+            if (reason !== null) fixedReason = reason.replace(new RegExp('\'', 'g'), '`');
+            Ban.find({ where: { userID: user.id } })
+              .on('success', (ban) => {
+                if (ban) {
+                  Ban.update({
+                    reason: fixedReason,
+                  }).catch((err) => console.error(err));
+                } else {
+                  Ban.create({
+                    userID: user.id,
+                    serverID: message.guild.id,
+                    userTag: user.tag,
+                    reason: fixedReason,
+                  }).catch((err) => console.error(err));
+                }
+              });
           });
         }).then(() => msg.edit({ embed: new RichEmbed().setAuthor('Done!', 'https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678134-sign-check-512.png') }));
     })
