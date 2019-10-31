f2e65b75f897d34dec940714388e07b020fa6411
Phil <philip.schaefer0@gmail.com>
2019-11-01 00:47:07 +0100

    added referneces

--- migrations/20191030192013-create_BanAssignedTag_table.js
+++ migrations/20191030192013-create_BanAssignedTag_table.js
@@ -3,11 +3,18 @@ module.exports = {
     banID: {
       type: Sequelize.INTEGER(11),
       allowNull: false,
-      primaryKey: true,
+      references: {
+        model: 'Bans',
+        key: 'banID',
+      },
     },
     tagID: {
       type: Sequelize.INTEGER(11),
       allowNull: false,
+      references: {
+        model: 'Tags',
+        key: 'tagID',
+      },
     },
     createdAt: Sequelize.DATE,
     updatedAt: Sequelize.DATE,
