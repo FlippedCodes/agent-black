be0051a9dc99c728c9e1697c42e7b82897d3728a
Phil <philip.schaefer0@gmail.com>
2019-11-01 01:44:09 +0100

    added contact

--- commands/help.js
+++ commands/help.js
@@ -5,6 +5,7 @@ module.exports.run = async (client, message, args, config) => {
   let embed = new RichEmbed()
     .setTitle('Halp')
     .setColor(message.member.displayColor)
+    .setDescription('For additional Info, please ask `Phil | Flipper#3621`')
     .addField(
       `\`${config.prefix}help\``,
       'Shows this list of commands', true,
