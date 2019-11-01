77c97517e8337a0271e7a9f9f3b1771c0e0ff518
Phil <philip.schaefer0@gmail.com>
2019-11-01 01:17:38 +0100

    inital init

---
+++ functions/FUNC_checkServer.js
@@ -0,0 +1,9 @@
+module.exports.run = async (client, config) => {
+  client.user.setStatus('online');
+  client.user.setActivity(`with ${config.prefix}help`)
+    .then(() => console.log('Set status!'));
+};
+
+module.exports.help = {
+  name: 'FUNC_checkServer',
+};
