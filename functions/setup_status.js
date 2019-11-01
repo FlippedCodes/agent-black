7c0c95cb5fb212a31cc55a8decc8a3e138f6e3c2
Phil <philip.schaefer0@gmail.com>
2019-11-01 01:06:36 +0100

    added module name to log message

--- functions/setup_status.js
+++ functions/setup_status.js
@@ -1,7 +1,8 @@
 module.exports.run = async (client, config) => {
+  console.log(`[${module.exports.help.name}] Setting status...`);
   client.user.setStatus('online');
   client.user.setActivity(`with ${config.prefix}help`)
-    .then(() => console.log('Set status!'));
+    .then(() => console.log(`[${module.exports.help.name}] Status set!`));
 };
 
 module.exports.help = {
