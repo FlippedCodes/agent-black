94ea59d0afc2aa45a931afebb3ac8d006c22337b
Phil <philip.schaefer0@gmail.com>
2019-11-02 14:45:55 +0100

    disabled status message

--- functions/setup_status.js
+++ functions/setup_status.js
@@ -1,5 +1,7 @@
 module.exports.run = async (client, config) => {
-  console.log(`[${module.exports.help.name}] Setting status...`);
+  if (!config.env.get('inDev')) {
+    console.log(`[${module.exports.help.name}] Setting status...`);
+  } else return console.log(`[${module.exports.help.name}] Bot is in debugging-mode and will post the bot status-message`);
   client.user.setStatus('online');
   client.user.setActivity(`with ${config.prefix}help`)
     .then(() => console.log(`[${module.exports.help.name}] Status set!`));
