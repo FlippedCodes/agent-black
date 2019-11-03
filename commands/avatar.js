dfc28a55cea12261c121480ec2b581a7d9c65104
Phil <philip.schaefer0@gmail.com>
2019-11-03 09:24:13 +0100

    rewritten code

--- commands/avatar.js
+++ commands/avatar.js
@@ -13,21 +13,19 @@ module.exports.run = async (client, message, args, config) => {
 
   // check if user is charing a server
   if (!await client.functions.get('FUNC_checkID').run(userID, client, 'user')) {
+    // TODO: when lookup is a function, retrieve pic from that
     messageFail(client, message, `The user with the ID \`${userID}\` doesn't share a server with this bot or doesn't exist. Running \`${config.prefix}lookup\` command for you.`);
     client.commands.get('lookup')
       .run(client, message, args, config);
     return;
   }
 
-  message.guild.fetchMember(userID)
-    .then((member) => {
-      if (member.user.avatarURL) {
-        const embed = new RichEmbed()
-          .setColor(member.displayColor)
-          .setImage(member.user.avatarURL);
-        message.channel.send({ embed });
-      } else messageFail(client, message, 'This user doesn\'t have a profile picture');
-    });
+  const user = await client.users.find((user) => user.id === userID);
+
+  if (!user.avatarURL) messageFail(client, message, 'This user doesn\'t have a profile picture');
+
+  const embed = new RichEmbed().setImage(user.avatarURL);
+  message.channel.send({ embed });
 };
 
 module.exports.help = {
