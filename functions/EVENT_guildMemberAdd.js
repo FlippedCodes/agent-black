61a40b04cca98650d2e86c02fca4641ff00be863
Phil <philip.schaefer0@gmail.com>
2019-11-09 21:05:10 +0100

    inital init

---
+++ functions/EVENT_guildMemberAdd.js
@@ -0,0 +1,30 @@
+const Ban = require('../database/models/Ban');
+
+const ParticipatingServer = require('../database/models/ParticipatingServer');
+
+module.exports.run = async (member) => {
+
+  // // getting newly added ban
+  // guild.fetchBan(user)
+  //   .then(async (ban) => {
+  //     const userID = user.id;
+  //     const userTag = user.tag;
+  //     const serverID = guild.id;
+  //     const reason = ban.reason;
+  //     let fixedReason = reason;
+  //     if (reason !== null) fixedReason = reason.replace(new RegExp('\'', 'g'), '`');
+  //     const [banEntry] = await Ban.findOrCreate({
+  //       where: { userID, serverID },
+  //       defaults: { userTag, reason: fixedReason },
+  //     }).catch(errHander);
+  //     if (!banEntry.isNewRecord) {
+  //       Ban.update({ reason: fixedReason, userTag },
+  //         { where: { userTag, userID, serverID } })
+  //         .catch(errHander);
+  //     }
+  //   });
+};
+
+module.exports.help = {
+  name: 'EVENT_guildMemberAdd',
+};
