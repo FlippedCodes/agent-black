d29143fc57f9b6ceb84b7f34114c9bda9bb381ed
Phil <philip.schaefer0@gmail.com>
2019-11-23 20:03:58 +0100

    implemented userBanned into code

--- functions/EVENT_guildBanAdd.js
+++ functions/EVENT_guildBanAdd.js
@@ -7,15 +7,16 @@ module.exports.run = async (guild, user) => {
       const userID = user.id;
       const userTag = user.tag;
       const serverID = guild.id;
+      const userBanned = true;
       const reason = ban.reason;
       let fixedReason = reason;
       if (reason !== null) fixedReason = reason.replace(new RegExp('\'', 'g'), '`');
       const [banEntry] = await Ban.findOrCreate({
         where: { userID, serverID },
-        defaults: { userTag, reason: fixedReason },
+        defaults: { userTag, reason: fixedReason, userBanned },
       }).catch(errHander);
       if (!banEntry.isNewRecord) {
-        Ban.update({ reason: fixedReason, userTag },
+        Ban.update({ reason: fixedReason, userTag, userBanned },
           { where: { userTag, userID, serverID } })
           .catch(errHander);
       }
