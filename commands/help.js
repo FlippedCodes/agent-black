d54f29c02f7e098a224549507e21c0723d86d1e8
Phil <philip.schaefer0@gmail.com>
2020-01-12 09:10:58 +0100

    added notes

--- commands/help.js
+++ commands/help.js
@@ -1,11 +1,12 @@
 const { RichEmbed } = require('discord.js');
 
 module.exports.run = async (client, message, args, config) => {
-  // prepare title and user CMDs
+  // prepare title and desc for embed
   let embed = new RichEmbed()
     .setTitle('Halp')
     .setColor(message.member.displayColor)
     .setDescription('For additional Info, please ask `Phil | Flipper#3621`');
+  // creating embed fields for every command
   client.commands.forEach((CMD) => {
     embed.addField(
       `\`${config.prefix}${CMD.help.name} ${CMD.help.usage || ''}\``,
