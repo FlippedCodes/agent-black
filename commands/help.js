c123355a4c452a45aa32050ed100d2fbfecf7ca3
Phil <philip.schaefer0@gmail.com>
2020-01-12 09:09:23 +0100

    added commandlist

---
+++ commands/help.js
@@ -0,0 +1,27 @@
+const { RichEmbed } = require('discord.js');
+
+module.exports.run = async (client, message, args, config) => {
+  // prepare title and user CMDs
+  let embed = new RichEmbed()
+    .setTitle('Halp')
+    .setColor(message.member.displayColor)
+    .setDescription('For additional Info, please ask `Phil | Flipper#3621`');
+  client.commands.forEach((CMD) => {
+    embed.addField(
+      `\`${config.prefix}${CMD.help.name} ${CMD.help.usage || ''}\``,
+      CMD.help.desc, false,
+    );
+  });
+  // set footer
+  // disabled due to design choice
+  // embed
+  //   .setFooter(message.client.user.tag, message.client.user.displayAvatarURL)
+  //   .setTimestamp();
+  message.channel.send(embed);
+  return;
+};
+
+module.exports.help = {
+  name: 'help',
+  desc: 'Shows a list of commands.',
+};
