3929af08c8e6400cb994118b2f6f8c0d470d5010
Phil <philip.schaefer0@gmail.com>
2020-11-24 02:11:16 +0100

    renaed and moved to functions

---
+++ functions/STARTUP_DBConnection.js
@@ -0,0 +1,38 @@
+const Sequelize = require('sequelize');
+
+const testToken = '../config/config.json';
+
+const config = require('../config/main.json');
+
+module.exports.run = () => {
+  console.log('[DB] Connecting...');
+  let database;
+  let user;
+  let password;
+  let host;
+  if (config.env.get('inDev')) {
+    const DBCredentials = require(testToken).development;
+    database = DBCredentials.database;
+    user = DBCredentials.username;
+    password = DBCredentials.password;
+    host = DBCredentials.host;
+  } else {
+    database = process.env.DBNameAgentBlack;
+    user = process.env.DBNameAgentBlack;
+    password = process.env.DBPasswAgentBlack;
+    host = process.env.DBHost;
+  }
+  const sequelize = new Sequelize(
+    database, user, password, { host, dialect: 'mysql', logging: config.env.get('inDev') },
+  );
+  console.log('[DB] Connected!');
+
+  // DEPRECATED: gets passed along outside of run function
+  // module.exports = module.exports.help;
+
+  global.sequelize = sequelize;
+};
+
+module.exports.help = {
+  name: 'STARTUP_DBConnection',
+};
