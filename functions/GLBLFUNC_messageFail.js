fe26a755218106b08a2db4416506acd8295679e8
Phil <philip.schaefer0@gmail.com>
2020-12-03 02:10:33 +0100

    updated called function

--- functions/GLBLFUNC_messageFail.js
+++ functions/GLBLFUNC_messageFail.js
@@ -2,7 +2,7 @@ global.messageFail = (message, body) => module.exports.run(message, body);
 
 module.exports.run = async (message, body) => {
   const client = message.client;
-  client.functions.get('FUNC_MessageEmbedMessage')
+  client.functions.get('FUNC_richEmbedMessage')
     .run(client.user, message.channel, body, '', 16449540, false)
     .then((msg) => msg.delete({ timeout: 10000 }));
 };
