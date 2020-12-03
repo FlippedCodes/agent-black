8807e3845c1d493e7d9a20b53e6477583d8d3bd0
Phil <philip.schaefer0@gmail.com>
2020-12-03 03:24:12 +0100

    code cleanup

--- functions/FUNC_richEmbedMessage.js
+++ functions/FUNC_richEmbedMessage.js
@@ -4,10 +4,10 @@ module.exports.run = async (user, channel, body, title, color, footer) => {
   // needs to be local as settings overlap from dofferent embed-requests
   const embed = new MessageEmbed();
 
-  if (footer) embed.setFooter(footer);
   if (body) embed.setDescription(body);
   if (title) embed.setTitle(title);
   if (color) embed.setColor(color);
+  if (footer) embed.setFooter(footer);
 
   return channel.send(embed);
 };
