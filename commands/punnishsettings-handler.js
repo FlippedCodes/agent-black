d3fe09112625098e468bab7da514acdef5c26178
Phil <philip.schaefer0@gmail.com>
2020-12-03 01:22:55 +0100

    added function sub commands function handler

--- commands/punnishsettings-handler.js
+++ commands/punnishsettings-handler.js
@@ -1,8 +1,34 @@
+// prepares command usage message
+function CommandUsage(prefix, cmdName, subcmd) {
+  return `Command usage: 
+    \`\`\`${prefix}${cmdName} ${subcmd}\`\`\``;
+}
+
+// creates a embed messagetemplate for failed actions
+function messageFail(message, body) {
+  const client = message.client;
+  client.functions.get('FUNC_MessageEmbedMessage')
+    .run(client.user, message.channel, body, '', 16449540, false)
+    .then((msg) => msg.delete({ timeout: 10000 }));
+}
+
 // is used to configure settings
 // if setting is not set, use default from config
 module.exports.run = async (client, message, args, config) => {
-  // check permissions (Admin)
-  // command handler for subcommands: list (list settings), enable, pointLifetime, listReasons, addReason, removeReason, listPunnishment, addPunnishment, removePunnishment
+  // TODO: check permissions (Servermanager)
+  // check DM
+  if (message.channel.type === 'dm') return messageFail(message, 'This comamnd is for servers only.');
+  // check if user is teammember
+  if (!message.member.roles.cache.find(({ id }) => id === config.teamRole)) return messageFail(message, 'You don\'t have access to this command! òwó');
+  const [subcmd] = args;
+  const commandValues = ['use', 'forceReason', 'pointLifetime', 'listReasons', 'addReason', 'removeReason', 'listPunnishment', 'addPunnishment', 'removePunnishment'];
+  const currentCMD = module.exports.help;
+  if (commandValues.includes(subcmd)) {
+    client.functions.get(`CMD_${currentCMD.name}_${subcmd}`)
+      .run(client, message, args, config, MessageEmbed);
+  } else {
+    messageFail(message, CommandUsage(config.prefix, currentCMD.name, commandValues.join('|')));
+  }
 };
 
 module.exports.help = {
