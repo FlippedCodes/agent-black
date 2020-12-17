b11c446f453a8014724fcacd8f66438be36e41d4
Phil <philip.schaefer0@gmail.com>
2020-12-17 02:23:58 +0100

    moved functions and value checking

--- functions/CMD_punishsettings_enable.js
+++ functions/CMD_punishsettings_enable.js
@@ -1,6 +1,23 @@
 const ServerSetting = require('../database/models/ServerSetting');
 
+// prepares command usage message
+function CommandUsage(prefix, cmdName, subcmd) {
+  return `Command usage: 
+    \`\`\`${prefix}${cmdName} ${subcmd}\`\`\``;
+}
+
 // adds server if not existent
+async function createServer(serverID) {
+  const created = await ServerSetting.findOrCreate({ where: { serverID } }).catch(errHander);
+  return created;
+}
+
+// checks is server is on list
+async function checkServer(serverID) {
+  const found = await ServerSetting.findOne({ where: { serverID } })
+    .catch(errHander);
+  return found;
+}
 
 // enables points system
 async function enablePointsSystem(serverID) {
@@ -18,22 +35,21 @@ async function disablePointsSystem(serverID) {
   return disabled;
 }
 
-// checks if a DB state is already set
-async function checkPointsSystem(serverID, pointsSystemEnabled) {
-  const found = await ServerSetting.findOne({ where: { serverID, pointsSystemEnabled } })
-    .catch(errHander);
-  return found;
-}
-
-// checks is server is on list
-async function checkServer(serverID, pointsSystemEnabled) {
-  const found = await ServerSetting.findOne({ where: { serverID } })
-    .catch(errHander);
-  return found;
-}
-
 module.exports.run = async (client, message, args, config) => {
-
+  const serverID = message.guild.id;
+  const [subcmd, enable] = args;
+  const lowercaseEnable = enable.toLowerCase();
+  // check if value is a bool
+  if (!(lowercaseEnable === 'true' || lowercaseEnable === 'false')) {
+    messageFail(message, CommandUsage(config.prefix, module.exports.help.parent, 'enable TRUEORFALSE'));
+    return;
+  }
+  // check if server exists in list and create it
+  const serverExist = await checkServer(serverID);
+  if (!serverExist) await createServer(serverID);
+  // set value
+  if (lowercaseEnable === 'true') await enablePointsSystem(serverID);
+  else await disablePointsSystem(serverID);
 };
 
 module.exports.help = {
