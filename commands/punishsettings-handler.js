ce9f1821bde6daf1a2af5bc931ac5d6274c3c5e4
Phil <philip.schaefer0@gmail.com>
2020-12-17 02:23:21 +0100

    checked logic to trigger right

--- commands/punishsettings-handler.js
+++ commands/punishsettings-handler.js
@@ -20,17 +20,17 @@ module.exports.run = async (client, message, args, config) => {
   // check DM
   if (message.channel.type === 'dm') return messageFail(message, 'This comamnd is for servers only.');
   // check if user is teammember
+  // TODO: update message
   if (!message.member.roles.cache.find(({ id }) => id === config.teamRole)) return messageFail(message, 'You don\'t have access to this command!');
   const [subcmd] = args;
   const commandValues = ['enable', 'forceReason', 'pointLifetime', 'listReasons', 'addReason', 'removeReason', 'listPunishment', 'addPunishment', 'removePunishment'];
   const currentCMD = module.exports.help;
-  if (subcmd === 'enable' || !await checkFeature(serverID)) return messageFail(message, `To use the comamnds, you need to enable the feature first: \`${CommandUsage(config.prefix, currentCMD.name, 'enable true')}\``);
   if (commandValues.includes(subcmd)) {
-    client.functions.get(`CMD_${currentCMD.name}_${subcmd}`)
-      .run(client, message, args, config);
-  } else {
-    messageFail(message, CommandUsage(config.prefix, currentCMD.name, commandValues.join('|')));
-  }
+    if (subcmd === 'enable' || await checkFeature(message.guild.id)) {
+      client.functions.get(`CMD_${currentCMD.name}_${subcmd}`)
+        .run(client, message, args, config);
+    } else messageFail(message, `To use the comamnds, you need to enable the feature first: \n${CommandUsage(config.prefix, currentCMD.name, 'enable true')}`);
+  } else messageFail(message, CommandUsage(config.prefix, currentCMD.name, commandValues.join('|')));
 };
 
 module.exports.help = {
