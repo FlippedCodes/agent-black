ff5d766956a658f96f77af509acd3eca370be718
Phil <philip.schaefer0@gmail.com>
2021-02-25 19:39:44 +0100

    added cached users to prevent overusage of db

--- functions/FUNC_userTagRecord.js
+++ functions/FUNC_userTagRecord.js
@@ -1,12 +1,16 @@
 const UserIDAssociation = require('../database/models/UserIDAssociation');
 
+const cachedUsers = [];
+
 module.exports.run = async (userID, userTag) => {
   if (userTag.indexOf('#0000') !== -1) return;
+  if (cachedUsers.find((id) => id === userID)) return;
   UserIDAssociation.findOrCreate({
     where: { userID },
     defaults: { userTag },
   })
     .catch(errHander);
+  cachedUsers.push(userID);
 };
 
 module.exports.help = {
