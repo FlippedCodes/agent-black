1761a3ef91fd1bde2e3b9f0c69fe7d0ae0103a5d
Phil <philip.schaefer0@gmail.com>
2021-04-21 22:27:00 +0200

    added todo

--- commands/alias.js
+++ commands/alias.js
@@ -23,23 +23,26 @@ async function checkFeature(serverID) {
 // if setting is not set, use default from config
 module.exports.run = async (client, message, args, config) => {
   // command usame checking
+  // ckeck if both ids are valid
   // command handler
   // add, remove
   if (message.channel.type === 'dm') return messageFail(message, 'This comamnd is for servers only.');
   // check if user is teammember
   if (!message.member.roles.cache.find(({ id }) => id === config.teamRole)) return messageFail(message, `You are not authorized to use \`${config.prefix}${module.exports.help.name}\``);
-  const [subcmd] = args;
-  const commandValues = ['manualAdd', 'manualRemove', 'stats', 'userStats(callsLookup)', 'add', 'remove', 'list', 'listPunishments'];
-  const currentCMD = module.exports.help;
-  if (commandValues.toLowerCase().includes(subcmd)) {
-    if (subcmd === 'enable' || await checkFeature(message.guild.id)) {
-      client.functions.get(`CMD_${currentCMD.name}_${subcmd}`)
-        .run(client, message, args, config);
-    } else messageFail(message, `To use the comamnds, you need to enable the feature in this server first!\n${CommandUsage(config.prefix, currentCMD.name, 'enable true')}`);
-  } else messageFail(message, CommandUsage(config.prefix, currentCMD.name, commandValues.join('|')));
+  const [mainUser, aliasUser] = args;
+  // DISABLED: might get added later
+  // const commandValues = ['add', 'remove'];
+  // const currentCMD = module.exports.help;
+  // if (commandValues.toLowerCase().includes(subcmd)) {
+  //   if (subcmd === 'enable' || await checkFeature(message.guild.id)) {
+  //     client.functions.get(`CMD_${currentCMD.name}_${subcmd}`)
+  //       .run(client, message, args, config);
+  //   } else messageFail(message, `To use the comamnds, you need to enable the feature in this server first!\n${CommandUsage(config.prefix, currentCMD.name, 'enable true')}`);
+  // } else messageFail(message, CommandUsage(config.prefix, currentCMD.name, commandValues.join('|')));
 };
 
 module.exports.help = {
   name: 'alias',
+  usage: 'MAINUSERID ALIASUSERID',
   desc: 'Add an alias for secound accounts.',
 };
