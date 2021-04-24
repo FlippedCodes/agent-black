d9430dd10a650e4b1034055469446aeb4fe905f4
Phil <philip.schaefer0@gmail.com>
2021-04-24 19:54:17 +0200

    cleanup

--- functions/EVENT_guildBanRemove.js
+++ functions/EVENT_guildBanRemove.js
@@ -9,9 +9,8 @@ module.exports.run = async (guild, user) => {
   // setting userBanned value to false for existing ban
   const userID = user.id;
   const serverID = guild.id;
-  const userBanned = false;
   // update ban-DB entry
-  Ban.update({ userBanned },
+  Ban.update({ userBanned: false },
     { where: { userID, serverID } })
     .catch(errHandler);
 };
