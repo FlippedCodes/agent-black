a2fcb1b6b00e5bb706ca7a8fe84e401b436ed035
Phil <philip.schaefer0@gmail.com>
2021-05-01 22:28:38 +0200

    added note to run checkallbans

--- functions/CMD_guild_setup.js
+++ functions/CMD_guild_setup.js
@@ -47,7 +47,7 @@ module.exports.run = async (client, message, args, config, prefix) => {
         // post outcome
         if (serverAdded) {
           messageSuccess(message,
-            `\`${serverName}\` with the ID \`${serverID}\` got added to / updated in the participating Servers list.\nYou can now use all the other commands in this server.`);
+            `\`${serverName}\` with the ID \`${serverID}\` got added to / updated in the participating Servers list.\nYou can now use all the other commands in this server.\nConsider running \`${prefix}checkallusers\` once in your log channel.`);
         } else {
           messageFail(message,
             `An active server entry for \`${serverName}\` with the ID \`${serverID}\` already exists! If you want to change info, remove it first with \`${prefix}${module.exports.help.parent} disable\``);
