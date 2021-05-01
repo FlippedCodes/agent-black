0814c38e9ba40e259c0c8d81f87737060a999b78
Phil <philip.schaefer0@gmail.com>
2021-05-01 16:01:04 +0200

    moved help to own field

--- commands/help.js
+++ commands/help.js
@@ -4,8 +4,7 @@ module.exports.run = async (client, message, args, config, prefix) => {
   // prepare title and desc for embed
   const embed = new MessageEmbed()
     .setTitle('Halp')
-    .setColor(message.member.displayColor)
-    .setDescription('For additional Info, please ask `Phil | Flipper#3621`');
+    .setColor(message.member.displayColor);
   // creating embed fields for every command
   client.commands.forEach((CMD) => {
     embed.addField(
@@ -13,6 +12,8 @@ module.exports.run = async (client, message, args, config, prefix) => {
       CMD.help.desc, false,
     );
   });
+  embed.addField('Need more help?', `
+  Join the help server here: https://discord.gg/QhfnAWgEMS`);
   message.channel.send(embed);
   return;
 };
