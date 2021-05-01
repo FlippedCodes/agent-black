017eb488f583d756dc164523a68e4aae54bfff8b
Phil <philip.schaefer0@gmail.com>
2021-05-01 23:14:19 +0200

    fixed output message

--- commands/lookup.js
+++ commands/lookup.js
@@ -149,9 +149,9 @@ async function postLookup(client, message, ID) {
 }
 
 module.exports.run = async (client, message, args, config, prefix) => {
-  // check permissions if user has teamrole and if send in DMs
+  // check permissions if user has teamrole
   if (!await client.functions.get('FUNC_checkPermissionsDB').run(message.author.id, 'staff', message.guild.id, message.member)) {
-    messageFail(message, `You are not authorized to use \`${prefix}${module.exports.help.name} ${subcmd}\``);
+    messageFail(message, `You are not authorized to use \`${prefix}${module.exports.help.name}\``);
     return;
   }
 
