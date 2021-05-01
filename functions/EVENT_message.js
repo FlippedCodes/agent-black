7a0b26f8bebb0262bcfd39be40e3abd8d021976e
Phil <philip.schaefer0@gmail.com>
2021-05-01 20:45:46 +0200

    checkin if guild is blocked

--- functions/EVENT_message.js
+++ functions/EVENT_message.js
@@ -1,3 +1,11 @@
+// checks if server is marked as blocked
+async function checkServer(serverID) {
+  const ParticipatingServer = require('../database/models/ParticipatingServer');
+  const found = await ParticipatingServer.findOne({ where: { serverID, blocked: true } })
+    .catch((err) => console.error(err));
+  return found;
+}
+
 module.exports.run = async (client, message, config) => {
   client.functions.get('FUNC_userTagRecord').run(message.author.id, message.author.tag);
   // return if unwanted
@@ -23,8 +31,16 @@ module.exports.run = async (client, message, config) => {
   let serverID = null;
   if (message.channel.guild) serverID = message.channel.guild.id;
   const mainCMD = command.slice(prefix.length).toLowerCase();
+  // commands to let through, when guild is blocked
+  const infoCMDs = ['help', 'about', 'ping'];
+  // check if blocked
+  if (!infoCMDs.includes(mainCMD) && await checkServer(serverID)) {
+    messageFail(message, 'It seems your server got blocked from the bot usage. If you want to know the reason and/or want to appeal, feel free to join the server linked in the help command.');
+    return;
+  }
   // commands to block, when guild has not been setup yet
   const mgmtCMDs = ['alias', 'ban', 'broadcast', 'eval', 'lookup', 'maintainer', 'warn', 'guildmgr'];
+  // check if active and if its a management command
   if (mgmtCMDs.includes(mainCMD) && !await client.functions.get('FUNC_checkServer').run(serverID, false)) {
     messageFail(message, `You need to setup the server first before yu can use this command.\nPlease run \`${prefix}guild setup\`.\n If you need help, please view the respective wiki article here (https://github.com/FlippedCode/agent-black/wiki/Adding-the-Bot) or join our support server`);
     return;
