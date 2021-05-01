03b8905b6a4864a535503947163ab7c3d8c1dfaf
Phil <philip.schaefer0@gmail.com>
2021-05-01 19:11:59 +0200

    added cacheclearing

--- functions/FUNC_userTagRecord.js
+++ functions/FUNC_userTagRecord.js
@@ -1,16 +1,27 @@
+const config = require('../config/main.json');
+
 const UserIDAssociation = require('../database/models/UserIDAssociation');
 
-const cachedUsers = [];
+const cachedUsers = new Set();
+
+function timeout(id) {
+  cachedUsers.add(id);
+  setTimeout(() => cachedUsers.delete(id), config.functions.userTagRecord.maxCacheTimeout);
+}
 
 module.exports.run = async (userID, userTag) => {
+  // return in discriminator is "#0000"
   if (userTag.indexOf('#0000') !== -1) return;
-  if (cachedUsers.find((id) => id === userID)) return;
+  // check if user has said anything in the last 3hrs
+  if (cachedUsers.has(userID)) return;
+  // if not add it
+  timeout(userID);
+  // and add entry to DB if not yet existent
   UserIDAssociation.findOrCreate({
     where: { userID },
     defaults: { userTag },
   })
     .catch(errHandler);
-  cachedUsers.push(userID);
 };
 
 module.exports.help = {
