6b4b80bf1db0809d54d6c5e6c5b28c63b38b458b
Phil <philip.schaefer0@gmail.com>
2021-05-01 19:12:24 +0200

    renamed commands

---
+++ functions/CMD_guild_setup.js
@@ -0,0 +1,48 @@
+const ParticipatingServer = require('../database/models/ParticipatingServer');
+
+// adds a server to the ParticipatingServers table
+async function addServer(serverID, logChannelID, teamRoleID, serverName) {
+  await ParticipatingServer.destroy({ limit: 1, where: { serverID, active: false } });
+  const added = await ParticipatingServer.findOrCreate(
+    {
+      where: { serverID },
+      defaults: {
+        logChannelID, teamRoleID, serverName, active: true,
+      },
+    },
+  ).catch((err) => console.error(err));
+  const created = await added[1];
+  return created;
+}
+
+module.exports.run = async (client, message, args, config, prefix) => {
+  // get subcmd from args
+  const [subcmd, logChannelID, teamRoleID] = args;
+  // check provided information
+  if (!logChannelID || !teamRoleID) {
+    messageFail(message,
+      `Command usage: 
+      \`\`\`${prefix}${module.exports.help.name} ${subcmd} ${logChannelID || 'LOG-CHANNELID'} TEAMROLEID\`\`\``);
+    return;
+  }
+  if (!await client.functions.get('FUNC_checkID').run(logChannelID, client, 'channel')) {
+    messageFail(message, `The channel with the ID \`${logChannelID}\` doesn't exist!`);
+    return;
+  }
+  const serverName = message.guild.name;
+  const serverID = message.guild.id;
+  const serverAdded = await addServer(serverID, logChannelID, teamRoleID, serverName);
+  // post outcome
+  if (serverAdded) {
+    messageSuccess(message,
+      `\`${serverName}\` with the ID \`${serverID}\` got added to / updated in the participating Servers list.\n you can now use all the other commands with this server.`);
+  } else {
+    messageFail(message,
+      `An active server entry for \`${serverName}\` with the ID \`${serverID}\` already exists! If you want to change info, remove it first.`);
+  }
+};
+
+module.exports.help = {
+  name: 'CMD_guild_setup',
+  parent: 'guild',
+};
