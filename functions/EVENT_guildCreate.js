d63da75356e1140be47bed161b5d2603f86d2346
Phil <philip.schaefer0@gmail.com>
2021-08-10 00:46:11 +0200

    added first, setup message

--- functions/EVENT_guildCreate.js
+++ functions/EVENT_guildCreate.js
@@ -19,7 +19,23 @@ async function addServerEntry(serverID, serverName) {
 
 module.exports.run = async (client, guild) => {
   const serverID = guild.id;
+  // check if was alreads added: add a server entry in the DB
   if (!await getServerEntry(client, serverID)) await addServerEntry(serverID, guild.name);
+  // message owner about adding the bot and how to procceed
+  const owner = await guild.fetchOwner();
+  const embed = new MessageEmbed()
+    .setTitle('Hello World!')
+    .setFooter('Only you recieved the message.')
+    .setDescription(`Thanks for adding me to your delightful server.
+Before you expect anything from me, I need you to complete some more steps before I get completely functional.
+
+Please tell me, where to log new members and what server role belongs to the team/staff. When you got the information, fill it into the command below and run it in any channel.
+\`a!guild setup [logChannelID] [teamRoleID]\`
+Example: \`a!guild setup 123456789123456 987654321987654\`
+
+After you run the command you need to confirm the Terms of Service and you are good to go!
+If you need any assistance, feel free to join our Discord server and we gladly help you out.`);
+  owner.send(embed);
   // add all bans to DB
   const allBans = await guild.fetchBans(true);
   allBans.forEach(async ({ user, reason }) => {
