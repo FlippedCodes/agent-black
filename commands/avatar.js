08bac884b0d4c01e863b8be41d7600c7e1c5f18d
Phil <philip.schaefer0@gmail.com>
2021-11-21 00:16:59 +0100

    upgraded

---
+++ commands/avatar.js
@@ -0,0 +1,19 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports.run = async (interaction) => {
+  // needs to be local as settings overlap from dofferent embed-requests
+  const embed = new MessageEmbed();
+  const command = interaction.options;
+  // get user and ID
+  const user = command.getUser('user', true);
+  const pfp = user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 });
+  embed.setAuthor(user.tag, null, pfp);
+  embed.setImage(pfp);
+
+  interaction.reply({ embeds: [embed] });
+};
+
+module.exports.data = new CmdBuilder()
+  .setName('avatar')
+  .setDescription('Retrieves the profile picture of the provided user ID.')
+  .addUserOption((option) => option.setName('user').setDescription('Provide a user to get the avatar from.').setRequired(true));
