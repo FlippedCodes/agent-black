d08b40b9606d672057560ef8137006e6d7cfd642
Phil <philip.schaefer0@gmail.com>
2021-11-21 13:10:04 +0100

    added editReply function

--- functions/richEmbedMessage.js
+++ functions/richEmbedMessage.js
@@ -1,6 +1,6 @@
 const { MessageEmbed } = require('discord.js');
 
-module.exports.run = async (interaction, body, title, color, footer, ephemeral) => {
+module.exports.run = async (interaction, body, title, color, footer, ephemeral, edit) => {
   // needs to be local as settings overlap from dofferent embed-requests
   const embed = new MessageEmbed();
 
@@ -9,10 +9,15 @@ module.exports.run = async (interaction, body, title, color, footer, ephemeral) 
   if (color) embed.setColor(color);
   if (footer) embed.setFooter(footer);
 
-  return interaction.reply({
+  const options = {
     embeds: [embed],
     ephemeral: String(ephemeral) ? ephemeral : true,
-  });
+  };
+
+  if (edit === undefined || edit) {
+    return interaction.editReply(options).catch(ERR);
+  }
+  return interaction.reply(options).catch(ERR);
 };
 
 module.exports.data = {
