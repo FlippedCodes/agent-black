3114f03ebeba6550092cb12ad0bc17d42ff817de
Phil <philip.schaefer0@gmail.com>
2021-11-22 19:31:41 +0100

    upgraded

---
+++ commands/help.js
@@ -0,0 +1,16 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports.run = async (interaction) => {
+  const embed = new MessageEmbed()
+    .setTitle('Halp')
+    .setColor(interaction.member.displayColor)
+    .setDescription('This command is deprecated, please use discord embedded slash-commands feature instead.')
+    .addField('Still need help?', `
+  Read the wiki here: https://github.com/FlippedCode/agent-black/wiki
+  or join our server here: https://discord.gg/QhfnAWgEMS`);
+  return reply(interaction, { embeds: [embed] });
+};
+
+module.exports.data = new CmdBuilder()
+  .setName('help')
+  .setDescription('Shows a list of commands. [Deprecated]');
