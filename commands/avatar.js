19bc733183c8491489a13d44e68489381191e8d0
Phil <philip.schaefer0@gmail.com>
2021-11-22 20:04:28 +0100

    added server pfp funtionality

--- commands/avatar.js
+++ commands/avatar.js
@@ -2,15 +2,25 @@ const { MessageEmbed } = require('discord.js');
 
 module.exports.run = async (interaction) => {
   // needs to be local as settings overlap from dofferent embed-requests
-  const embed = new MessageEmbed();
+  const embedDefault = new MessageEmbed();
   const command = interaction.options;
   // get user and ID
-  const user = command.getUser('user', true);
-  const pfp = user.displayAvatarURL({ format: 'png', dynamic: true, size: 4096 });
-  embed.setAuthor(user.tag, null, pfp);
-  embed.setImage(pfp);
+  const rawUser = command.get('user', true);
+  const user = rawUser.user;
+  const member = rawUser.member;
+  const defaultPFP = user.displayAvatarURL({ format: 'png', dynamic: true, size: 4096 });
+  embedDefault.setAuthor(user.tag, null, defaultPFP)
+    .setImage(defaultPFP);
+  await reply(interaction, { embeds: [embedDefault] });
 
-  reply(interaction, { embeds: [embed] });
+  if (member) {
+    const embedServer = new MessageEmbed();
+    const serverPFP = member.displayAvatarURL({ format: 'png', dynamic: true, size: 4096 });
+    embedServer.setAuthor(member.nickname, null, serverPFP)
+      .setImage(serverPFP)
+      .setFooter('Server profile picture');
+    interaction.followUp({ embeds: [embedServer] });
+  }
 };
 
 module.exports.data = new CmdBuilder()
