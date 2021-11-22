33ce2798a1360d2c068980d88d8290b578cec909
Phil <philip.schaefer0@gmail.com>
2021-11-22 18:36:56 +0100

    remade comamnd structure

--- commands/maintainer.js
+++ commands/maintainer.js
@@ -6,22 +6,20 @@ module.exports.run = async (interaction) => {
     messageFail(interaction, `You are not authorized to use \`${module.exports.data.name}\``);
     return;
   }
-  const subName = interaction.options.getSubcommand();
-  client.commands.get(`${interaction.commandName}_${subName}`).run(interaction, Maintainer);
+  const subName = interaction.options.getString('action', true);
+  client.commands.get(`${module.exports.data.name}_${subName}`).run(interaction, Maintainer);
 };
 
 module.exports.data = new CmdBuilder()
   .setName('maintainer')
   .setDescription('Manages the maintainers. [MAINTAINER ONLY]')
-  .addSubcommand((subcommand) => subcommand
-    .setName('add')
-    .setDescription('Adds an entry.')
-    .addUserOption((option) => option.setName('user').setDescription('Provide a user to to add.').setRequired(true)))
-  .addSubcommand((subcommand) => subcommand
-    .setName('remove')
-    .setDescription('Allow access to nsfw rooms.')
-    .addUserOption((option) => option.setName('user').setDescription('Provide a user to to remove.').setRequired(true)))
-  .addSubcommand((subcommand) => subcommand
-    .setName('info')
-    .setDescription('Change the DoB of an entry.')
-    .addUserOption((option) => option.setName('user').setDescription('Provide a user you want to know more about.').setRequired(true)));
+  .addUserOption((option) => option.setName('user').setDescription('Provide a user you want to edit.').setRequired(true))
+  .addStringOption((option) => option
+    .setName('action')
+    .setDescription('What do you want to do with this user?')
+    .addChoices([
+      ['Add user', 'add'],
+      ['Remove user', 'remove'],
+      ['Disply info about user', 'info'],
+    ])
+    .setRequired(true));
