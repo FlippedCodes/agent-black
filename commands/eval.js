3021f70dc6bd0ec2f80f03cd37ad370de034836a
Phil <philip.schaefer0@gmail.com>
2021-11-22 19:08:05 +0100

    better security and upgraded

---
+++ commands/eval.js
@@ -0,0 +1,30 @@
+const clean = (text) => {
+  if (typeof (text) === 'string') {
+    return text.replace(/`/g, `\`${String.fromCharCode(8203)}`)
+      .replace(/@/g, `@${String.fromCharCode(8203)}`)
+      .replace(process.env.DCtoken, '****NOPE****')
+      .replace(process.env.DBpassword, '****NOPE****')
+      .replace(process.env.DBhost, '****NOPE****')
+      .replace(process.env.DBusername, '****NOPE****');
+  }
+  return text;
+};
+
+module.exports.run = async (interaction) => {
+  // check owner permissions
+  if (interaction.user.id !== '172031697355800577') return messageFail(interaction, `You are not authorized to use \`${module.exports.data.name}\``);
+  const code = interaction.options.getString('codeline', true);
+  // if (code.indexOf('DCtoken' || 'process.env.DCtoken' || 'token' || 'client') !== -1) return message.channel.send('Do you think its that easy?\nSry, but cant give you my key...');
+  try {
+    let evaled = eval(code);
+
+    if (typeof evaled !== 'string') { evaled = require('util').inspect(evaled); }
+
+    messageSuccess(interaction, `\`\`\`xl\n${clean(evaled)}\n\`\`\``, null, true);
+  } catch (err) { messageFail(interaction, `\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``); }
+};
+
+module.exports.data = new CmdBuilder()
+  .setName('eval')
+  .setDescription('Command used to run snippets of code. [OWNER ONLY].')
+  .addStringOption((option) => option.setName('codeline').setDescription('Commandline to execute').setRequired(true));
