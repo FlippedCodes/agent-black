b0f439601ecb59f8180ec3d56113560565ed70ce
Phil <philip.schaefer0@gmail.com>
2021-11-22 01:42:57 +0100

    code cleanup

--- commands/about.js
+++ commands/about.js
@@ -1,11 +1,5 @@
 const fs = require('fs');
 
-// creates a embed messagetemplate for failed text retrieving and posts error message into log
-function error(err, interaction) {
-  ERR(err);
-  messageFail(interaction, 'Something went wrong, try again another time!');
-}
-
 module.exports.run = async (interaction) => {
   fs.readFile(config.commands.about.text, 'utf8', (err, body) => {
     if (err) {
