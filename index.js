053f50ee52d76c898c725f84a3447288c4aedbfd
Phil <philip.schaefer0@gmail.com>
2022-01-27 01:47:23 +0100

    [fix] its functions nod a command

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ client.on('interactionCreate', async (interaction) => {
   if (!await interaction.inGuild()) return messageFail(interaction, 'The bot is for server-use only.');
 
   // autocomplete hanlder
-  if (interaction.isAutocomplete()) return client.commands.get('EVENT_autocomplete').run(interaction).catch(ERR);
+  if (interaction.isAutocomplete()) return client.functions.get('EVENT_autocomplete').run(interaction).catch(ERR);
   // command handler
   if (interaction.isCommand()) {
     const command = client.commands.get(DEBUG ? interaction.commandName.replace('_dev', '') : interaction.commandName);
