c1733d6548f975004e374b7c0dca1295e1dba6dd
Phil <philip.schaefer0@gmail.com>
2022-04-09 05:30:00 +0200

    [update] status message

--- functions/SETUP/status.js
+++ functions/SETUP/status.js
@@ -2,9 +2,9 @@ module.exports.run = async () => {
   if (DEBUG) return;
   console.log(`[${module.exports.data.name}] Setting status...`);
   await client.user.setStatus('online');
-  const membercount = await client.guilds.cache.reduce((previousCount, currentGuild) => previousCount + currentGuild.memberCount, 0);
-  // TODO: update activity message
-  await client.user.setActivity(`${membercount} members in Devoravore`, { type: 'WATCHING' });
+  // const membercount = await client.guilds.cache.reduce((previousCount, currentGuild) => previousCount + currentGuild.memberCount, 0);
+  // TODO: update activity message, maybe with servercount, once reached 100 servers
+  await client.user.setActivity('with /help');
   console.log(`[${module.exports.data.name}] Status set!`);
 };
 
