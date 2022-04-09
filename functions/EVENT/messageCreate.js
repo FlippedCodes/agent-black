f26b45453695261335cccc66fba9c000f0d255b1
Phil <philip.schaefer0@gmail.com>
2022-04-10 01:33:15 +0200

    [add] comment

--- functions/EVENT/messageCreate.js
+++ functions/EVENT/messageCreate.js
@@ -13,6 +13,7 @@ module.exports.run = async (message) => {
   if (message.author.bot) return;
   if (!message.content.startsWith('a!')) return;
   const sentMessage = await message.channel.send('Hi there! I have been upgraded to Slash-Commands (v.3.0.0) and no longer support the old prefix of `a!` (Blame Discord). Please use the new `/` instead!');
+  // For some reason that isnta-deletes the message?
   // await sentMessage.delete({ timeout: 20000 });
 };
 
