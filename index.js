e24a76c0c656e3a198e4dc433654f2e42ff3a4fd
Phil <philip.schaefer0@gmail.com>
2022-04-09 20:47:08 +0200

    [add] todo

--- index.js
+++ index.js
@@ -93,6 +93,8 @@ client.on('messageCreate', (message) => client.functions.get('EVENT_messageCreat
 // itneraction is triggered (command, autocomplete, etc.)
 client.on('interactionCreate', async (interaction) => {
   // only guild command
+  // TODO: check if neseccary, because this seems to be a useless intent 'DIRECT_MESSAGES'
+  // TODO: maybe check if command deployment to DM can be supressed
   if (!await interaction.inGuild()) return messageFail(interaction, 'The bot is for server-use only.');
 
   // autocomplete hanlder
