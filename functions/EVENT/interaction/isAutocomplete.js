8e4a41512e3e36a9abf7fa393370c6b9da4a9922
Phil <philip.schaefer0@gmail.com>
2022-04-14 07:43:35 +0200

    [fix] dev not getting removed causing crash

--- functions/EVENT/interaction/isAutocomplete.js
+++ functions/EVENT/interaction/isAutocomplete.js
@@ -1,6 +1,6 @@
 module.exports.run = async (interaction) => {
-  const commandName = DEBUG ? interaction.commandName.replace('_dev', '') : interaction.commandName;
-  console.log(commandName);
+  // const commandName = DEBUG ? interaction.commandName.replace('_dev', '') : interaction.commandName;
+  const commandName = interaction.commandName.replace('_dev', '');
   return client.functions.get(`AUTOCOMPLETE_${commandName}`).run(interaction).catch(ERR);
 };
 
