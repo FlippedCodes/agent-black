dea67d1df2e9ebaff69ba052b7a3a35c59fc3675
Phil <philip.schaefer0@gmail.com>
2022-09-09 18:21:47 +0200

    [fix] activity upgrade v14

--- functions/SETUP/status.js
+++ functions/SETUP/status.js
@@ -1,14 +1,14 @@
+const { ActivityType } = require('discord.js');
+
 module.exports.run = async () => {
   if (DEBUG) return;
   console.log(`[${module.exports.data.name}] Setting status...`);
-  await client.user.setStatus('online');
-  // const membercount = await client.guilds.cache.reduce((previousCount, currentGuild) => previousCount + currentGuild.memberCount, 0);
-  // TODO: update activity message, maybe with servercount, once reached 100 servers
-  await client.user.setActivity('with /help');
+  const servercount = await client.guilds.cache.size;
+  await client.user.setActivity(`over ${servercount} servers!`, { type: ActivityType.Watching });
   console.log(`[${module.exports.data.name}] Status set!`);
 };
 
 module.exports.data = {
   name: 'status',
-  callOn: '-',
+  callOn: 'setup',
 };
